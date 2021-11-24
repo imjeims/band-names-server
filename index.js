f1f4aa8c878dfedbe3fcc4c23dba6081a4f84ce7
jamesquinteroc <james.quintero@ceiba.com.co>
2021-11-23 19:27:06 -0500

    add a public directory

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const publicPath = path.resolve(__dirname, 'public');
+
+app.use(express.static(publicPath));
 
 app.listen(3000, (err) => {
     if (err) throw new Error(err);
 
     console.log('server running on port:', 3000)
-})
+});
