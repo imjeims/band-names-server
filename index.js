9dcebf0c59066809783f876fb11f0fc558204c5a
jamesquinteroc <james.quintero@ceiba.com.co>
2021-11-23 20:21:36 -0500

    add socket.io configuration

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+// Express app
 const app = express();
+
+// Node server
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// Socket messages
+io.on('connection', client => {
+    console.log('client connected')
+
+    client.on('disconnect', () => { console.log('client disconnected') });
+});
+
+// Public path
 const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, (err) => {
+server.listen(process.env.PORT, (err) => {
     if (err) throw new Error(err);
 
     console.log('server running on port:', process.env.PORT)
